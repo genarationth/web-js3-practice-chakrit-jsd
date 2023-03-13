c7bade14f018299ccc53b12598d657a05951210d
chakrit-jsd <chakritabv@gmail.com>
2023-03-13 16:24:43 +0700

    done

---
+++ script.js
@@ -0,0 +1,28 @@
+const add = (a, b) => {
+
+    return a + b  
+}
+const substract = (a, b) => {
+
+    return a - b  
+}
+const multiply = (a, b) => {
+
+    return a * b  
+}
+const divide = (a, b) => {
+
+    return a / b  
+}
+
+console.log(add(1, 3))
+console.log(substract(1, 3))
+console.log(multiply(1, 3))
+console.log(divide(1, 3))
+
+
+const inform = (name, location, hobby) => {
+    console.log(`Hi, my name ${name} I live in ${location} and enjoy ${hobby}`)
+}
+
+inform('John', 'Bangkok', 'run')
